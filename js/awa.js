d5877e1c57a7d45dc0765efebe1e842a1ff5070b
mc506lw <3462689887@qq.com>
2024-04-13 11:50:25 +0800

    分开
    分开力

--- js/awa.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.parallelogram').forEach(function(parallelogram) {
-    parallelogram.addEventListener('click', function() {
-      this.style.width = this.style.width === '200px' ? '100px' : '200px';
-    });
-  });
